58a7579ca10de7f8f0ce7b32b65b1ab0ff3e4fba
Fabricio Augusto Oliveira Bastos <168025112+Gutobastos@users.noreply.github.com>
2025-06-12 14:50:28 -0300

    Delete src/main/java/br/com/alura/literalura/dto directory

--- src/main/java/br/com/alura/literalura/dto/LivroDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.literalura.dto;
-
-public record LivroDTO(Long id,
-                       String titulo,
-                       String autores,
-                       String idioma,
-                       int download) {
-}
